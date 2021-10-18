4c116f43824f63c529a5b6473d3851a7e6c2be0f
EC2 Default User <ec2-user@ip-172-31-72-156.ec2.internal>
2021-10-18 06:07:58 +0000

    modified

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -1,18 +1,42 @@
 class BooksController < ApplicationController
   
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
   
   def index
+   @books = Book.all
+   @book = Book.new
+   
   end
   
   def show
+   @book = Book.find(params[:id])
   end
   
   def edit
+   @book = Book.find(params[:id])
+  end
+  
+  def update
+   @book = Book.find(params[:id])
+   @book.update(book_params)
+   redirect_to book_path(@book.id)
   end
   
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
 end
--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to:'homes#top'
-  resources :books, only:[:create, :index, :show, :edit, :destroy]
+  resources :books, only:[:create, :index, :update, :show, :edit, :destroy]
   
 end
